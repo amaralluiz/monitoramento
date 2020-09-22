f78fdf24679f68f3043624ae89ce7cff9ccd4333
Luiz Amaral <alaluizamaral@gmail.com>
2020-09-22 10:29:23 -0300

    Criacao da página. Criação de endpoint para envio do json.

--- main.py
+++ main.py
@@ -1,11 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from model import Computador
+import psutil
+import platform
+from datetime import datetime
+
+
+def get_size(bytes, suffix='B'):
+    factor = 1024
+    for unit in ["", "K", "M", "G", "T", "P"]:
+        if bytes < factor:
+            return f"{bytes:.2f}{unit}{suffix}"
+        bytes /= factor
+
+
+uname = platform.uname()
+nome = uname.node
+svmem = psutil.virtual_memory()
+memoria_total = get_size(svmem.total)
+memoria_em_uso = get_size(svmem.used)
+memoria_disponivel = get_size(svmem.available)
+
+computador = Computador(nome, memoria_total,
+                        memoria_em_uso, memoria_disponivel)
 
 app = Flask(__name__)
 
 
+computador_dicionario = computador.__dict__
+
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', computador=computador)
 
 
-app.run()
+@app.route('/api')
+def api():
+    return jsonify(computador_dicionario)
+
+
+app.run(debug=True)
---
+++ model.py
@@ -0,0 +1,6 @@
+class Computador:
+    def __init__(self, nome, memoria_total, memoria_em_uso, memoria_disponivel):
+        self.nome = nome
+        self.memoria_total = memoria_total
+        self.memoria_em_uso = memoria_em_uso
+        self.memoria_disponivel = memoria_disponivel
